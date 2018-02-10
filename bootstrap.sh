b1750dfce0d8eb693df34ea977d93ac764a14058
Thomas Monguillon <thomas.monguillon@orange.com>
2018-02-10 13:17:32 +0100

    Noauth param and local_ip as env variable. Remove embedded ovs support

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,12 +11,12 @@ ML2_CONFIG_FILE=/etc/neutron/plugins/ml2/ml2_conf.ini
 
 SQL_SCRIPT=${SQL_SCRIPT:-/opt/neutron.sql}
 MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD:-$MYSQL_ENV_MYSQL_ROOT_PASSWORD}
-MYSQL_HOST=${MYSQL_HOST:-mysql}
-until mysql -uroot -p$MYSQL_ROOT_PASSWORD -h $MYSQL_HOST -e 'select 1'; do
+MYSQL_HOST=${MYSQL_HOST:-127.0.0.1}
+until mysql -uroot -p$MYSQL_ROOT_PASSWORD -h$MYSQL_HOST -e 'select 1'; do
   >&2 echo "MySQL is unavailable - sleeping"
-  sleep 1
+  sleep 3
 done
-mysql -uroot -p$MYSQL_ROOT_PASSWORD -h $MYSQL_HOST <$SQL_SCRIPT
+mysql -uroot -p$MYSQL_ROOT_PASSWORD -h$MYSQL_HOST <$SQL_SCRIPT
 
 
 neutron-db-manage \
@@ -29,6 +29,6 @@ neutron-db-manage \
   --subproject networking-sfc \
   upgrade head
 
-/usr/bin/ovsdb-tool create /etc/openvswitch/conf.db /usr/share/openvswitch/vswitch.ovsschema
-#/etc/init.d/openvswitch-switch start
+/usr/bin/ovs-vsctl emer-reset
+sed -i "s#^local_ip.*=.*#local_ip = ${LOCAL_IP}#" $ML2_CONFIG_FILE
 /usr/bin/supervisord
